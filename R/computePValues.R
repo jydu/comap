3b038e16198cd5e47496cf536fe3c9b3898df3d7
Julien Y. Dutheil <jydu@users.noreply.github.com>
2011-02-23 15:17:10 +0000

    Added script to build example archives.

--- R/computePValues.R
+++ R/computePValues.R
@@ -2,7 +2,7 @@
 # 06/06/2006
 # 15/11/2007
 # 04/08/2007
-# 04/12/2009
+# 17/02/2011
 
 # ----------------------------------------------------------------------------------------------------------------
 # EDITABLE SECTION:
@@ -14,51 +14,46 @@
 # 1) Clustering output
 
 all.data.files<-list()
-all.data.files[["Simple"]]    <-"../Coe/Simple/Myo_groups.csv"
-all.data.files[["Grantham"]]  <-"../Coe/Grantham/Myo_groups.csv"
-all.data.files[["Volume"]]    <-"../Coe/Volume/Myo_groups.csv"
-all.data.files[["Polarity"]]  <-"../Coe/Polarity/Myo_groups.csv"
-all.data.files[["Charge"]]    <-"../Coe/Charge/Myo_groups.csv"
+all.data.files[["Simple"]]    <-"../CoMap/Simple/Myo_groups.csv"
+all.data.files[["Grantham"]]  <-"../CoMap/Grantham/Myo_groups.csv"
+all.data.files[["Volume"]]    <-"../CoMap/Volume/Myo_groups.csv"
+all.data.files[["Polarity"]]  <-"../CoMap/Polarity/Myo_groups.csv"
+all.data.files[["Charge"]]    <-"../CoMap/Charge/Myo_groups.csv"
 
 
 # 2) Simulations
 
 all.sim.files<-list()
-all.sim.files[["Simple"]]    <-"../Coe/Simple/Myo_groups_simulations.csv"
-all.sim.files[["Grantham"]]  <-"../Coe/Grantham/Myo_groups_simulations.csv"
-all.sim.files[["Volume"]]    <-"../Coe/Volume/Myo_groups_simulations.csv"
-all.sim.files[["Polarity"]]  <-"../Coe/Polarity/Myo_groups_simulations.csv"
-all.sim.files[["Charge"]]    <-"../Coe/Charge/Myo_groups_simulations.csv"
+all.sim.files[["Simple"]]    <-"../CoMap/Simple/Myo_simulations.csv"
+all.sim.files[["Grantham"]]  <-"../CoMap/Grantham/Myo_simulations.csv"
+all.sim.files[["Volume"]]    <-"../CoMap/Volume/Myo_simulations.csv"
+all.sim.files[["Polarity"]]  <-"../CoMap/Polarity/Myo_simulations.csv"
+all.sim.files[["Charge"]]    <-"../CoMap/Charge/Myo_simulations.csv"
 
 # 3) Output files:
-# Type I test:
-output.file<-"Myo_groups_predictions_pvalues.csv"
+output.file1<-"Myo_predictions_pvalues.csv"
 
 #Sliding windows sizes:
 window.Nmin<-0.2
 
 # General options:
 
-#Maximum p-value level or groups in the output files.
-#Use NA if you don't want to filter the output.
+#Maximum group size to test:
+maxgs<-10
+#Maximum p-value level or groups in the output files:
 level<-0.05
 #Minimum number of simulated points required for computing p-value:
 min.nobs<-1000
-#Log file ("" = terminal)
-logFile<-"Cliques.txt"
-#FDR (NA if you don't want to perform a FDR analysis):
-fdr<-0.05
-#Number of replicates to use for computing FDR:
-nfdr<-10
-#Grid preprocessing (very recommended for pairwise analysis)
-gridpp<-TRUE
-
-# Clustering options (will have no effect for pairwise analyses):
-
-#Maximum group size to test:
-maxgs<-10
 #Correction for nested groups:
 cng<-TRUE
+#Log file ("" = terminal)
+logFile<-"Cliques.txt"
+#FDR:
+fdr<-0.05
+#Number of replicates to use for computing FDR:
+nfdr<-100
+#Grid preprocessing (very recommended for pairwise analysis)
+gridpp<-TRUE
 
 # ----------------------------------------------------------------------------------------------------------------
 # END OF EDITABLE SECTION
@@ -89,7 +84,7 @@ all.pred<-list()
 for(m in methods)
 {
   cat("Testing method '", m, "'.\n", sep="")
-  all.pred[[m]]<-format.pred(all.data[[m]], all.sim[[m]], 2:maxgs, window.Nmin, min.nobs, gridpp, m, level, cng, paste(m,logFile,sep="_"), fdr, nfdr)
+  all.pred[[m]]<-format.pred(all.data[[m]], all.sim[[m]], 2:maxgs, window.Nmin, min.nobs, gridpp, method=m, level=level, cng=cng, logFile=paste(m,logFile,sep="_"), fdr=fdr, nfdr=nfdr)
 }
 
 # Merging all results:
